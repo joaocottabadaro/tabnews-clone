4409b4e0d2922b4389db35db4e8c1e41c51aaf7b
joaocottabadaro <joaobadaro@gmail.com>
2023-08-17 20:25:25 +0000

    add lottie lib e icone pagina de construcao

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import React from "react";
+import Lottie from "lottie-react";
+import underConstruction from "../assets/underConstruction.json";
+
 function Home() {
-  return <h1>Hello world !!</h1>;
+  return (
+    <Lottie
+      animationData={underConstruction}
+      loop={true}
+      style={{ width: "500px", margin: "20vh auto" }}
+    />
+  );
 }
 
 export default Home;
